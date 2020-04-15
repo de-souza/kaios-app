f1a43d63055f3c14a2b03ad7cf05513287e78d48
de-souza <43355143+de-souza@users.noreply.github.com>
2020-04-16 00:50:22 +0200

    Add navigation support and simplify code

--- style/main.js
+++ style/main.js
@@ -1,1 +1,28 @@
-document.getElementsByClassName("List-item")[0].focus();
+"use strict";
+
+document.getElementById("Autofocus").focus();
+document.addEventListener("keydown", listNavigation);
+
+function listNavigation(e) {
+  if (e.target.matches("li")) {
+    if (e.key == "ArrowUp" || e.key == "ArrowDown") {
+      e.preventDefault();
+      nextFocusedElment(e).focus();
+    }
+  }
+}
+
+function nextFocusedElment(e) {
+  switch(e.key) {
+  case "ArrowUp":
+    if (e.target.previousElementSibling)
+      return e.target.previousElementSibling;
+    else
+      return e.target.parentElement.lastElementChild;
+  case "ArrowDown":
+    if (e.target.nextElementSibling)
+      return e.target.nextElementSibling;
+    else
+      return e.target.parentElement.firstElementChild;
+  }
+}
