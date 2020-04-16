f7d72284051c23a7934a69349982a850dde1b46f
de-souza <43355143+de-souza@users.noreply.github.com>
2020-04-17 01:09:50 +0200

    Simplify code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,35 +7,39 @@ function menuNavigationListener(e) {
   if (e.target.matches(".Menu-item")) {
     switch(e.key) {
     case "ArrowUp":
-      return menuUp(e);
+      menuMove(e.target, previousMenuItem);
+      e.preventDefault();
+      break;
     case "ArrowDown":
-      return menuDown(e);
+      menuMove(e.target, nextMenuItem);
+      e.preventDefault();
+      break;
     }
   }
 }
 
-function menuUp(e) {
-  let newMenuItem = previousListItem(e.target.parentElement).firstElementChild;
+function menuMove(currentMenuItem, newMenuItemFunc) {
+  const newMenuItem = newMenuItemFunc(currentMenuItem);
   newMenuItem.focus();
   scrollToMenuItem(newMenuItem);
-  e.preventDefault();
 }
 
-function menuDown(e) {
-  let newMenuItem = nextListItem(e.target.parentElement).firstElementChild;
-  newMenuItem.focus();
-  scrollToMenuItem(newMenuItem);
-  e.preventDefault();
+function previousMenuItem(currentMenuItem) {
+  return previousItemInList(currentMenuItem.parentElement).firstElementChild;
 }
 
-function previousListItem(current) {
+function nextMenuItem(currentMenuItem) {
+  return nextItemInList(currentMenuItem.parentElement).firstElementChild;
+}
+
+function previousItemInList(current) {
   if (current.previousElementSibling)
     return current.previousElementSibling;
   else
     return current.parentElement.lastElementChild;
 }
 
-function nextListItem(current) {
+function nextItemInList(current) {
   if (current.nextElementSibling)
     return current.nextElementSibling;
   else
