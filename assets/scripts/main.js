59729aa2d0df3c5888fcd2836a610642ecd7a12d
de-souza <43355143+de-souza@users.noreply.github.com>
2020-04-17 01:24:11 +0200

    Use more explicit names

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,50 +7,49 @@ function menuNavigationListener(e) {
   if (e.target.matches(".Menu-item")) {
     switch(e.key) {
     case "ArrowUp":
-      menuMove(e.target, previousMenuItem);
+      focusMenuItem(previousMenuItem(e.target));
       e.preventDefault();
       break;
     case "ArrowDown":
-      menuMove(e.target, nextMenuItem);
+      focusMenuItem(nextMenuItem(e.target));
       e.preventDefault();
       break;
     }
   }
 }
 
-function menuMove(currentMenuItem, newMenuItemFunc) {
-  const newMenuItem = newMenuItemFunc(currentMenuItem);
+function focusMenuItem(newMenuItem) {
   newMenuItem.focus();
   scrollToMenuItem(newMenuItem);
 }
 
 function previousMenuItem(currentMenuItem) {
-  return previousItemInList(currentMenuItem.parentElement).firstElementChild;
+  return previousListItem(currentMenuItem.parentElement).firstElementChild;
 }
 
 function nextMenuItem(currentMenuItem) {
-  return nextItemInList(currentMenuItem.parentElement).firstElementChild;
+  return nextListItem(currentMenuItem.parentElement).firstElementChild;
 }
 
-function previousItemInList(current) {
-  if (current.previousElementSibling)
-    return current.previousElementSibling;
+function previousListItem(currentListItem) {
+  if (currentListItem.previousElementSibling)
+    return currentListItem.previousElementSibling;
   else
-    return current.parentElement.lastElementChild;
+    return currentListItem.parentElement.lastElementChild;
 }
 
-function nextItemInList(current) {
-  if (current.nextElementSibling)
-    return current.nextElementSibling;
+function nextListItem(currentListItem) {
+  if (currentListItem.nextElementSibling)
+    return currentListItem.nextElementSibling;
   else
-    return current.parentElement.firstElementChild;
+    return currentListItem.parentElement.firstElementChild;
 }
 
-function scrollToMenuItem(newMenuItem) {
-  const menuItemRect = newMenuItem.getBoundingClientRect();
-  const menuRect = newMenuItem.closest(".Menu").getBoundingClientRect();
+function scrollToMenuItem(menuItem) {
+  const menuItemRect = menuItem.getBoundingClientRect();
+  const menuRect = menuItem.closest(".Menu").getBoundingClientRect();
   if (menuItemRect.top < menuRect.top)
-    newMenuItem.scrollIntoView(true);
+    menuItem.scrollIntoView(true);
   else if (menuItemRect.bottom > menuRect.bottom)
-    newMenuItem.scrollIntoView(false);
+    menuItem.scrollIntoView(false);
 }
